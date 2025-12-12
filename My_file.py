87acd2cbde71ae41963f5b00df907b2aa21e8348
fassa108 <sambndeyefassa@gmail.com>
2025-12-12 15:36:06 +0000

    Modification du fichier My_file.py

--- My_file.py
+++ My_file.py
@@ -18,3 +18,25 @@ else:
         fact = fact *i
 
 print(f"Le factoriel de {nombre} vaut : {fact}")
+def Factoriel(nombre) :
+    fact = 1
+    if nombre == 0 : 
+        print(f"Le factoriel de {nombre} vaut : {fact}")
+    else:
+        for i in range(1,nombre + 1):
+            fact = fact *i
+    return fact
+
+while True:
+    nombre = input("veuillez saisir un nombre positif : ")
+    if not nombre.isnumeric :
+        print("Erreur!!! Le nombre doit etre positif!!!")
+    else :
+        nombre = int(nombre)
+        if nombre < 0 :
+            print("Erreur!!! Le nombre doit etre positif!!!")
+        else : 
+            break
+
+fact = Factoriel(nombre)
+print(f"Le factoriel de {nombre} vaut : {fact}")
